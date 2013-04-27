3ecee13e827dd04d9ef510afd6bc6f81188f7eb0
Gnuk <gnuk@mailoo.org>
2013-04-27 12:20:32 +0200

    Ajout de commentaires et licence pour le noeud

--- src/analyseur/Noeud.java
+++ src/analyseur/Noeud.java
@@ -1,17 +1,50 @@
 package analyseur;
 
+/*
+* Copyright (C) 2013, Anthony Rey and Patricio Saraiva
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/**
+* Gestion des noeuds d'un arbre
+* @author Patricio Saraiva
+* @author Anthony Rey <anthony.rey@etu.univ-savoie.fr>
+*/
 public class Noeud {
 	
 	private String racine;
 	private Noeud filsDroit;
 	private Noeud filsGauche;
 	
+	/**
+	 * Construit un nouveau noeud
+	 * @param racine La valeur du noeud
+	 * @param filsGauche Le fils gauche de ce noeud s'il existe
+	 * @param filsDroit Le fils droit de ce noeud s'il existe
+	 */
 	public Noeud(String racine, Noeud filsGauche, Noeud filsDroit){
 		this.racine = racine;
 		this.filsDroit = filsDroit;
 		this.filsGauche = filsGauche;
 	}
 	
+	/**
+	 * Construit une feuille sous forme de noeud sans fils
+	 * @param racine La valeur de la feuille
+	 */
 	public Noeud(String racine){
 		this.racine = racine;
 		this.filsDroit = null;
@@ -38,6 +71,9 @@ public class Noeud {
 		return racine;
 	}
 	
+	/**
+	 * Récupère l'arbre sous forme textuelle
+	 */
 	public String toString(){
 
 		if(this.filsDroit != null && this.filsGauche != null){
@@ -54,6 +90,9 @@ public class Noeud {
 		}
 	}
 	
+	/**
+	 * Affiche l'arbre sous forme textuelle
+	 */
 	public void show(){
 		System.out.println(this.toString());
 	}
